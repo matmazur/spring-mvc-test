5f9f213360e93f2814d47bea207ad8ce17c0c4df
Bingo Bobo <matmazur90@gmail.com>
2018-12-14 14:58:16 +0100

    changed AppInit to imeplent different interface with less config needed to be done, created WebConfig that implements Configurer interface, class now is part of Configuration in AppInit. Created Controller (servlet kinda) with one method sending to home jsp as the root page

--- src/main/java/com/matmazur/AppInit.java
+++ src/main/java/com/matmazur/AppInit.java
@@ -1,23 +1,31 @@
 package com.matmazur;
 
-import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.*;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration.*;
-
-public class AppInit implements WebApplicationInitializer {
+public class AppInit  extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return null;
+    }
 
     @Override
-    public void onStartup(ServletContext servletContext) throws ServletException {
-
-        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
-        context.setServletContext(servletContext);
-
-        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
-        Dynamic dispatcherConfig = servletContext.addServlet("dispatcher", dispatcherServlet);
-        dispatcherConfig.addMapping("/");
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{WebConfig.class};
     }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+//    @Override
+//    public void onStartup(ServletContext servletContext) throws ServletException {
+//
+//        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+//        context.setServletContext(servletContext);
+//
+//        DispatcherServlet dispatcherServlet = new DispatcherServlet(context);
+//        Dynamic dispatcherConfig = servletContext.addServlet("dispatcher", dispatcherServlet);
+//        dispatcherConfig.addMapping("/");
+//    }
 }
